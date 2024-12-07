7c23b146455666fb208ec5fa16ceb9626d922538
Johnny Dread <wulgaru@hispeed.ch>
2024-12-07 06:50:25 +0100

    readded permutions function from previous years

--- src/Common.kt
+++ src/Common.kt
@@ -1,3 +1,19 @@
 data class Point(val x: Int, val y: Int) {
     operator fun plus(other: Point) = Point(x + other.x, y + other.y)
 }
+
+fun <T> permutations(possibilities: List<T>, length: Int): List<List<T>> {
+    val result = mutableListOf<List<T>>()
+    fun generateCombination(current: List<T>) {
+        if (current.size == length) {
+            result.add(current)
+            return
+        }
+        for (p in possibilities) {
+            generateCombination(current + p)
+        }
+    }
+    generateCombination(listOf())
+
+    return result
+}
