60ecc9dd95f940db9fdd4da991cffae142c41afd
Johnny Dread <wulgaru@hispeed.ch>
2024-12-12 14:22:33 +0100

    small template update

--- src/DayXX.kt
+++ src/DayXX.kt
@@ -13,8 +13,8 @@ fun main() {
     val testInput = readInput("day%02d_test".format(day))
     val input = readInput("day%02d".format(day))
 
-    checkSolution("Part1", part1(testInput), 0)
-    checkSolution("Part1", part1(input), 0)
-    checkSolution("Part2", part2(testInput), 0)
-    checkSolution("Part2", part2(input), 0)
+    checkSolution("Part1 [Test]", part1(testInput), 0)
+    checkSolution("Part1 [Full]", part1(input), 0)
+    checkSolution("Part2 [Test]", part2(testInput), 0)
+    checkSolution("Part2 [Full]", part2(input), 0)
 }
